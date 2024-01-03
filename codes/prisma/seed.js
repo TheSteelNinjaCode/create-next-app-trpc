d704b385f63a59871be281c8e2afb17651258827
Jefferson <thesteelninjacode@gmail.com>
2024-01-03 15:17:01 -0600

    Refactor database seed script for PostgresSQL and MySQL

--- codes/prisma/seed.js
+++ codes/prisma/seed.js
@@ -11,21 +11,28 @@ const userData = [
 async function main() {
   // Prisma Seed Data for PostgresSQL
   // User
-  await prisma.user.deleteMany();
-  await prisma.user.createMany({ data: userData });
+  // Code below is for PostgresSQL
+  // ========================================
+  // await prisma.user.deleteMany();
+  // await prisma.user.createMany({ data: userData });
   // Reset sequence
   // users is the name of the table in the database
   // id_seq is the name of the sequence in the database that is auto increment
-  await prisma.$executeRaw`ALTER SEQUENCE users_id_seq RESTART WITH 1`;
-
+  // Code below is for PostgresSQL
+  // ========================================
+  // await prisma.$executeRaw`ALTER SEQUENCE users_id_seq RESTART WITH 1`;
   // Prisma Seed Data for MySQL
   // User
-  await prisma.user.deleteMany();
-  await prisma.user.createMany({ data: userData });
+  // Code below is for MySQL
+  // ========================================
+  // await prisma.user.deleteMany();
+  // await prisma.user.createMany({ data: userData });
   // Reset sequence
   // users is the name of the table in the database
   // id is the name of the column in the table that is auto increment
-  await prisma.$executeRaw`ALTER TABLE users AUTO_INCREMENT = 1`;
+  // Code below is for MySQL
+  // ========================================
+  // await prisma.$executeRaw`ALTER TABLE users AUTO_INCREMENT = 1`;
 }
 
 main()
